3e0fc845959b9bbd80aa5b5f5d96f0e224e3d6a6
Marko Dimitrijevic <mdimitrijevic84@gmail.com>
2026-03-05 23:55:55 +0100

    Preload Inter font.

--- includes/class-theme-supports.php
+++ includes/class-theme-supports.php
@@ -30,6 +30,22 @@ class Theme_Supports {
 	public static function init() {
 		// Fix for "Links do not have descriptive text".
 		\add_filter( 'render_block', array( __CLASS__, 'add_accessible_read_more' ), 10, 2 );
+
+		// Add this line to trigger the font preload.
+		add_action( 'wp_head', array( __CLASS__, 'preload_fonts' ), 5 );
+	}
+
+	/**
+	 * Preload the Inter variable font to prevent layout shifts.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return void
+	 */
+	public static function preload_fonts() {
+		?>
+		<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/inter/Inter-VariableFont_opsz,wght.ttf' ) ); ?>" as="font" type="font/ttf" crossorigin>
+		<?php
 	}
 
 	/**
