67e9cb47baf65ab811a95c6a61aade160c59099e
Marko Dimitrijevic <mdimitrijevic84@gmail.com>
2026-03-10 22:48:49 +0100

    Remove defer loading of main style.

--- includes/class-assets-loader.php
+++ includes/class-assets-loader.php
@@ -40,9 +40,6 @@ class Assets_Loader {
 
 		// Increase maximum size for CSS files to be inlined by WordPress.
 		\add_filter( 'styles_inline_size_limit', array( __CLASS__, 'inline_style_limit' ) );
-
-		// Defer loading of specified styles.
-		\add_filter( 'style_loader_tag', array( __CLASS__, 'defer_loading_of_front_css' ), 10, 2 );
 	}
 
 	/**
@@ -123,49 +120,6 @@ class Assets_Loader {
 		$new_limit = 61440;
 
 		return $new_limit;
-	}
-
-	/**
-	 * Filters the HTML link tags for specific stylesheets to implement deferred loading
-	 * via the 'rel="preload"' attribute and JavaScript onload fallback.
-	 *
-	 * This method prevents the CSS from being render-blocking initially, improving
-	 * page load performance, especially for non-critical styles
-	 *
-	 * @since 0.1.0
-	 *
-	 * @hooked style_loader_tag
-	 *
-	 * @param string $html   The full HTML link tag for the enqueued style.
-	 * @param string $handle The style's registered handle.
-	 * @return string The modified HTML link tag for deferred loading, or the original tag.
-	 */
-	public static function defer_loading_of_front_css( $html, $handle ) {
-		// Define an array of stylesheet handles for offload.
-		$offload_handles = array(
-			'beyond-fse-front-style',
-		// Add any other handles here.
-		);
-
-		// Check if the current stylesheet's handle is in our list.
-		if ( in_array( $handle, $offload_handles, true ) ) {
-			// Replace 'rel="stylesheet"' with the preload pattern.
-			// The onload attribute uses JS to switch rel='preload' back to rel='stylesheet' after the page loads.
-			// Note: Using &apos; for single quotes inside the attribute value for robust HTML output.
-			$preload_html = str_replace(
-				"rel='stylesheet'",
-				"rel='preload' as='style' onload=\"this.onload=null;this.rel='stylesheet'\"",
-				$html
-			);
-
-			// Include a noscript fallback for browsers without JavaScript enabled.
-			$noscript_fallback = "<noscript>{$html}</noscript>";
-
-			return $preload_html . $noscript_fallback;
-		}
-
-		// If the handle is not in our list, return the original HTML.
-		return $html;
 	}
 }
 
